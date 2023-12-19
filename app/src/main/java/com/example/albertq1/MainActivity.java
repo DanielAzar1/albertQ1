b8919194458d8a4825b24043b33164a3d9511835
Daniel azar <da6331@bs.amalnet.k12.il>
2023-12-19 09:22:05 +0200

    kjk

--- app/src/main/java/com/example/albertq1/MainActivity.java
+++ app/src/main/java/com/example/albertq1/MainActivity.java
@@ -5,18 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     Button btn;
+    TextView txtV;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.btn);
+        txtV = findViewById(R.id.txtV);
     }
 
     public void btn(View view) {
         btn = findViewById(R.id.btn);
-        btn.setText("oh yeah ive been clicked!");
+        btn.setText("oh yeah ive been clicked! ");
     }
 
 }
